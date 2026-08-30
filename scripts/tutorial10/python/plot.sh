e6e299f8f274d6357706906f847b2a44ba9e8938
hesitationIsDefeat <tanrioveronat@gmail.com>
2026-08-30 13:07:56 +0300

    feat (plot): Add config file copying mechanism to the tutorial results

--- scripts/tutorial10/python/plot.sh
+++ scripts/tutorial10/python/plot.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# ONAT: archive the config used for this run into the results folder, so reviewing
+# results later doesn't require cross-referencing scripts/tutorial10/config (which may
+# have changed since, e.g. for a later sweep). Kept as .xml/.properties (not renamed to
+# .txt) since that's the simpler default - rename below if a viewer/tool needs .txt.
+config_dir="../config"
+results_dir="../../../sim_results/tutorial10"
+mkdir -p "$results_dir"
+cp "$config_dir/default_config.properties" "$results_dir/"
+cp "$config_dir/edge_devices.xml" "$results_dir/"
+cp "$config_dir/applications.xml" "$results_dir/"
+
 # Array of python scripts to execute
 scripts=(
     "plotAvgFailedTask.py"
